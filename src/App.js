af04002c779543fd1af88f0409ec418ef19fc215
Yves Rey Badan <yrtech.innovations@gmail.com>
2024-12-12 14:35:31 +0800

    Commit portfolio repository

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        THIS IS A FULL STACK WEB DEVELOPER PORTFOLIO
+      </h1>
+    </div>
+  );
+}
+
+export default App;
